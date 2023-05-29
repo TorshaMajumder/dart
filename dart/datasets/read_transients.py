5cb435237652c16d17c94ae1f27c212bb76d8984
Torsha <tmajumder.work@gmail.com>
2023-05-29 16:25:58 +0530

    updated two files

--- dart/datasets/read_transients.py
+++ dart/datasets/read_transients.py
@@ -18,10 +18,18 @@ path_to_store = f"../transients"
 
 def main():
     #
-    # Declare a dictionary to store the fluxes
-    # with keys as - "g_band" and "r-band"
+    # Declare the variables
     #
-    flux = dict()
+    data = dict()
+    #
+    # Load the transient names
+    #
+    try:
+        with open(f"../transients/plasticc_labels_.pickle", 'rb') as f:
+            load_data = pickle.load(f)
+        labels = [k for k in load_data.keys()]
+    except Exception as e:
+        print(f"\n\nException Raised: {e}\n\n")
     #
     # Calculate the timesteps for each bands
     #
@@ -31,14 +39,13 @@ def main():
     # Declare the path to store the fluxes
     #
     # Iterate through all the files in the folder
-    # Store the fluxes for each band as a dictionary
+    # Store the fluxes for each band and concatenate them
     # under the folder /transients
     # The file name is - flux.pickle
     #
     try:
         path = f"{path_to_store}/plasticc_data/"
         filenames = os.listdir(path)
-
         #
         g_flux = np.zeros((len(filenames), timesteps))
         r_flux = np.zeros((len(filenames), timesteps))
@@ -48,20 +55,23 @@ def main():
             g_flux[i] = data["g_flux"].values.reshape((1, timesteps))
             r_flux[i] = data["r_flux"].values.reshape((1, timesteps))
         #
-        #
+        # Replace any NaNs with zero
         #
         g_flux[np.isnan(g_flux)] = 0
         r_flux[np.isnan(r_flux)] = 0
-        flux["g_band"] = g_flux
-        flux["r_band"] = r_flux
+        flux = np.concatenate((g_flux, r_flux), axis=1)
+        #
+        data["data"] = flux
+        data["labels"] = labels
+        #
     except Exception as e:
         print(f"\n\nException Raised: {e}\n\n")
-
     #
     # Store the dictionary as a pickle file
     #
     with open(f"{path_to_store}/flux.pickle", 'wb') as f:
-        pickle.dump(flux, f)
+        pickle.dump(data, f)
+    print(f"\n\nFluxes are generated and stored in the folder -- /{path_to_store}.\n\n")
 
 
 if __name__ == '__main__':
--- dart/datasets/transients.py
+++ dart/datasets/transients.py
@@ -5,13 +5,14 @@ import os
 import pickle
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
+from dart.connectors.feature_to_cluster import combine_features
 
 
 def load_latent_space(extract_type=None):
 
+    #combine_features(extract_type=extract_type)
+
     file_list = list()
-    scaler = StandardScaler()
 
     try:
         if os.path.exists('../latent_space_data/transients'):
@@ -30,6 +31,15 @@ def load_latent_space(extract_type=None):
                             data = pickle.load(file)
 
                 elif extract_type == "tsfresh":
+                    if f"{extract_type}.pickle" not in file_list:
+                        raise FileNotFoundError(f"\nFileNotFoundError: '{extract_type}.pickle' file "
+                                                f"doesn't exists in the folder - /latent_space_data/transients.\n")
+
+                    else:
+                        with open(f"../latent_space_data/transients/{extract_type}.pickle", 'rb') as file:
+                            data = pickle.load(file)
+
+                elif extract_type == "umap":
                     if f"{extract_type}.pickle" not in file_list:
                         raise FileNotFoundError(f"\nFileNotFoundError: '{extract_type}.pickle' file "
                                                 f"doesn't exists in the folder - /latent_space_data/transients.\n")
@@ -61,12 +71,10 @@ def load_latent_space(extract_type=None):
                                     f"k_pca, tsfresh, isomap,or vae, but got {extract_type}.\n")
 
                 if isinstance(data['data'], (np.ndarray, np.generic)):
-                    #data["data"] = scaler.fit_transform(data["data"])
                     return data
 
                 elif isinstance(data['data'], pd.DataFrame):
                     data['data'] = data['data'].to_numpy()
-                    #data["data"] = scaler.fit_transform(data["data"])
                     return data
 
 
@@ -87,11 +95,9 @@ def load_latent_space(extract_type=None):
         exit()
 
 
-
-
 if __name__ == '__main__':
 
-    data = load_latent_space(extract_type='k_pca')
+    data = load_latent_space(extract_type='umap')
 
 
 
